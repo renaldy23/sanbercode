e27adb442a060e573cbf16599783cd494ffb9fba
renaldy 23 <renaldyramadhan567@gmail.com>
2020-08-05 18:49:07 +0700

    Tugas Blade Selesai

--- blog - Tugas Laravel 04-08-2020/resources/views/table.blade.php
+++ blog - Tugas Laravel 04-08-2020/resources/views/table.blade.php
@@ -444,8 +444,8 @@
 	<script src="{{asset('/adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
 	<script src="{{asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 	<script>
-	  $(function () {
-	    $("#example1").DataTable();
-	  });
+	  // $(function () {
+	  //   $("#example1").DataTable();
+	  // });
 	</script>
 @endpush
